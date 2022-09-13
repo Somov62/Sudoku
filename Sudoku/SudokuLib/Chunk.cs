d2838eeb4df3ec2a5ea1d516fde971c80b8da1f0
Somov <misha2003980@gmail.com>
2022-09-13 20:37:14 +0300

    Добавил библиотеку, создающую судоку

---
+++ Sudoku/SudokuLib/Chunk.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SudokuLib
+{
+    public class Chunk
+    {
+        /// <summary>
+        /// Длина одного измерения чанка.
+        /// Площадь чанка = _size * _size
+        /// </summary>
+        private readonly int _size;
+        public Chunk(int size)
+        {
+            _size = size;
+            ChunkData = new List<Number>(size);
+            for (int i = 0; i < size * size; i++)
+            {
+                ChunkData.Add(new Number());
+            }
+        }
+
+        /// <summary>
+        /// Представление чанков в качестве
+        /// коллекции для удобного биндинга
+        /// </summary>
+        public List<Number> ChunkData { get; }
+
+        public Number this[int row, int column]
+        {
+            get => ChunkData[row * _size + column];
+            set => ChunkData[row * _size + column] = value;
+        }
+
+        public bool Validate()
+        {
+            return ChunkData.Where(p => p.IsValid == false).Count() == 0;
+        }
+
+        public int OpenedDigitsCount()
+        {
+            return ChunkData.Where(p => p.Value == 0).Count();
+        }
+    }
+}
---
+++ Sudoku/SudokuLib/Sudoku.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SudokuLib.Realization_1;
+
+namespace SudokuLib
+{
+    public class Sudoku
+    {
+        public Sudoku(int countChunksInDimension = 3, int difficultyLevel = 2)
+        {
+            CountChunksInDimension = countChunksInDimension;
+            ChunkMatrix = new Generator().GenerateSudoku(CountChunksInDimension, difficultyLevel);
+            ChunkList = MatrixToList();
+        }
+        public int CountChunksInDimension { get; set; }
+        public Chunk[,] ChunkMatrix { get; set; }
+        public List<Chunk> ChunkList { get; set; }
+
+        public bool Validate()
+        {
+            return ChunkList.Where(p => p.Validate() == false).Count() == 0;
+        }
+
+        private List<Chunk> MatrixToList()
+        {
+            List<Chunk> list = new List<Chunk>(CountChunksInDimension * CountChunksInDimension);
+            for (int i = 0; i < CountChunksInDimension; i++)
+            {
+                for (int j = 0; j < CountChunksInDimension; j++)
+                {
+                    list.Add(ChunkMatrix[i, j]);
+                }
+            }
+            return list;
+        }
+    }
+}
